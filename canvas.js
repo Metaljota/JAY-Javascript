b6525ccbb52897d5d9fed9e35e92d4496210ed7f
Jay <105905548+Metaljota@users.noreply.github.com>
2022-06-23 15:18:55 +1000

    The line is not working

--- canvas.js
+++ canvas.js
@@ -1,39 +1,58 @@
 let canvas = document.querySelector("#myCanvas");
 let ctx = canvas.getContext("2d");
-  document.querySelector("#drawCircle").addEventListener("click", draw);
+  ctx.fillStyle = 'white'; //background color for canvas
+  ctx.fillRect(0,0,canvas.width, canvas.height); //background color for canvas
   
-  function draw(){
+  document.querySelector("#drawCircle").addEventListener("click", draw);//calling funtion to draw circle
+  document.querySelector("#drawRectangle").addEventListener("click", draw2);
+  document.querySelector("#drawLine").addEventListener("click", draw3);
+  document.querySelector("#buttonID").addEventListener("click", changeColor);
+  document.querySelector("#palette").addEventListener("click", myPalette);
+ 
+
+  function changeColor(){
+    document.querySelector("#pID").style.color = "green";
+  }
+
+  //function for circle
+  function draw(){ 
     ctx.beginPath();
-    ctx.arc(100,100, 20, 0, 2 * Math.PI);
+    ctx.arc(document.querySelector("#x").value, document.querySelector("#y").value, document.querySelector("#slider").value, 0, 2 * Math.PI);
     ctx.strokeStyle = "black";
     ctx.stroke();
-    ctx.fillStyle = "blue";
+    ctx.fillStyle = document.querySelector("#palette").value; 
     ctx.fill();
   }
 
+  //function for rectangle
+  function draw2(){
+    ctx.beginPath();
+    ctx.rect(document.querySelector("#x2").value, document.querySelector("#y2").value, document.querySelector("#width").value, document.querySelector("#height").value);
+    ctx.fillStyle = document.querySelector("#palette2").value;
+    ctx.fill();
+    ctx.stroke();
+  }
+
+  function draw3(){
+    ctx.beginPath();
+    ctx.line(38, 31, 30, 200);
+    //ctx.moveTo(100,100);
+    //ctx.LineTo(300,100);
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = "red";
+    ctx.stroke();
+  }
+
+  /*function for triangle
+  function draw2(){
+    ctx.beginPath();
+    ctx.moveTo(75,75);
+    ctx.lineTo(10,75);
+    ctx.lineTo(10,25);
+    ctx.fillStyle = document.querySelector("#palette2").value;
+    ctx.fill();
+  }*/
 
 
-
-
-/*THE CANVAS AND SOME SHAPES
-
-/*window.onload = function(){
-  let canvas = document.getElementById("myCanvas");
-  let context = canvas.getContext("2d");
-  context.lineWidth = 15;// set the line width to 10 pixels
-  context.strokeStyle = "blue";// set the line color to blue
-    context.moveTo(100, canvas.height - 50);
-    context.bezierCurveTo(150, 10, 420, 10, 420, 180);
-  context.stroke();
-};
-
-/*context.quadraticCurveTo(canvas.width / 2, -50, canvas.width - 100, canvas.height - 50);*/
-
-/*context.arc(canvas.width / 2, canvas.height / 2 + 40, 80, 1.1 * Math.PI, 1.9 * Math.PI, false);*/
-
-  /*  context.moveTo(50, canvas.height - 50);// position the drawing cursor
-  context.lineTo(canvas.width - 50, 50);
-  context.stroke();
-      };*/
 
 
