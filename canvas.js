ea347a4a69af0960518ad51be759504e956550bd
Jay <105905548+Metaljota@users.noreply.github.com>
2022-06-28 13:21:01 +1000

    Webpage ready

--- canvas.js
+++ canvas.js
@@ -1,23 +1,78 @@
+// Jay Sierra 24 June 2022
+//This script provide the functions needed for the Abstract Art Competition website. 
+
 let canvas = document.querySelector("#myCanvas");
 let ctx = canvas.getContext("2d");
   ctx.fillStyle = 'white'; //background color for canvas
-  ctx.fillRect(0,0,canvas.width, canvas.height); //background color for canvas
+  ctx.fillRect(0,0,canvas.width, canvas.height); //background color for canvas continue
   
-  document.querySelector("#drawCircle").addEventListener("click", draw);//calling funtion to draw circle
-  document.querySelector("#drawRectangle").addEventListener("click", draw2);
-  document.querySelector("#drawLine").addEventListener("click", draw3);
-  document.querySelector("#buttonID").addEventListener("click", changeColor);
+  document.querySelector("#drawCircle").addEventListener("click", drawCircle);//calling a funtion to draw a circle
+  document.querySelector("#drawRectangle").addEventListener("click", drawRectangle);//calling a funtion to draw a rectangle
+  document.querySelector("#drawLine").addEventListener("click", drawLine);//calling a funtion to draw a line
+  document.querySelector("#language").addEventListener("click", ChangeLanguage);
   document.querySelector("#palette").addEventListener("click", myPalette);
- 
+  document.querySelector("#cnfpwd").addEventListener('change', validatePassword);
 
-  function changeColor(){
-    document.querySelector("#pID").style.color = "green";
+  function myPalette(){
+    console.log(document.querySelector("#palette").value)
   }
+ 
+  //Saving the art function
+  let button = document.getElementById('btn-download');
+  button.addEventListener('click', function (e) {
+      let dataURL = canvas.toDataURL('image/png');
+      button.href = dataURL;
+  });
+
+  //Function for changing language
+  function ChangeLanguage(){
+    if (document.querySelector("#language").value == "english"){
+      document.querySelector("#title").innerHTML = "Abstract Art Competition";
+      document.querySelector("#title").style.color = "green";
+      document.querySelector("#title2").innerHTML = "Welcome to the online Art Competition";
+      document.querySelector("#title3").innerHTML = "Instructions";
+      document.querySelector("#instructions").innerHTML = "Please draw you art in the canvas bellow using the provided tools.<br>When finished, sleect Save Art to download your art or Clear to start again.<br>Use the form to write down your personal details, upload your art file and click submit. Good Luck!!!";
+      document.querySelector("#drawCircle").innerHTML = "Click here to draw a Circle";
+      document.querySelector("#drawRectangle").innerHTML = "Click here to draw a Rectangle";
+      document.querySelector("#drawLine").innerHTML = "Click here to draw a Line";
+    }
+    else if (document.querySelector("#language").value == "spanish"){
+      document.querySelector("#title").innerHTML = "Competicion de Arte Abstracto";
+      document.querySelector("#title").style.color = "orange";
+      document.querySelector("#title2").innerHTML = "Bienvenidos a la competicion en linea de arte";
+      document.querySelector("#title3").innerHTML = "Instrucciones";
+      document.querySelector("#instructions").innerHTML = "Porfavor dibuje su arte an el siguiente lienzo con las herramientas presentes.<br>Cuando termine seleccione Save Art para descargar su arte o Clear para dibujar de nuevo.<br>Utilice el formulario para escribir sus datos personales, carge el archivo final y seleccione Submit. Buena Suerte!!!";
+      document.querySelector("#drawCircle").innerHTML = "Seleccione para dibujar Circulo";
+      document.querySelector("#drawRectangle").innerHTML = "Seleccione para dibujar Rectangulo";
+      document.querySelector("#drawLine").innerHTML = "Selecciones para dibujar linea";
+    }
+  } 
+
+  //to clear the canvas
+  document.getElementById("clear").addEventListener('click', function() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0,0,canvas.width, canvas.height);
+  }, false);
+
+  //an iteration for changing colour on "Instruction"  and wellcome text words. 
+  const titles = document.querySelectorAll(".titleA");
+  titles.forEach(mouse);
+  function mouse(currentValue, index, arr) {
+    arr[index].addEventListener ("mouseover", function(){
+      currentValue.style.color = "red";
+    });
+    arr[index].addEventListener ("mouseout", function(){
+      currentValue.style.color = "black";
+    });
+    }  
+   
 
   //function for circle
-  function draw(){ 
+  function drawCircle(){ 
     ctx.beginPath();
-    ctx.arc(document.querySelector("#x").value, document.querySelector("#y").value, document.querySelector("#slider").value, 0, 2 * Math.PI);
+    ctx.arc(document.querySelector("#x").value, document.querySelector("#y").value, 
+    document.querySelector("#slider").value, 0, 2 * Math.PI);
     ctx.strokeStyle = "black";
     ctx.stroke();
     ctx.fillStyle = document.querySelector("#palette").value; 
@@ -25,33 +80,42 @@ let ctx = canvas.getContext("2d");
   }
 
   //function for rectangle
-  function draw2(){
+  function drawRectangle(){
     ctx.beginPath();
-    ctx.rect(document.querySelector("#x2").value, document.querySelector("#y2").value, document.querySelector("#width").value, document.querySelector("#height").value);
+    ctx.rect(document.querySelector("#x2").value, document.querySelector("#y2").value, 
+    document.querySelector("#width").value, document.querySelector("#height").value);
     ctx.fillStyle = document.querySelector("#palette2").value;
     ctx.fill();
     ctx.stroke();
   }
 
-  function draw3(){
+  //funtion for line
+  function drawLine(){
+    ctx.strokeStyle = document.querySelector("#palette3").value;
+    ctx.lineWidth = document.querySelector("#slider2").value;
     ctx.beginPath();
-    ctx.line(38, 31, 30, 200);
-    //ctx.moveTo(100,100);
-    //ctx.LineTo(300,100);
-    ctx.lineWidth = 4;
-    ctx.strokeStyle = "red";
+    ctx.moveTo(document.querySelector("#x3").value, document.querySelector("#y3").value);
+    ctx.lineTo(document.querySelector("#x4").value, document.querySelector("#y4").value);
     ctx.stroke();
-  }
+    }
 
-  /*function for triangle
-  function draw2(){
-    ctx.beginPath();
-    ctx.moveTo(75,75);
-    ctx.lineTo(10,75);
-    ctx.lineTo(10,25);
-    ctx.fillStyle = document.querySelector("#palette2").value;
-    ctx.fill();
-  }*/
+  //function to validate the password
+    function validatePassword(){
+    if(document.querySelector("#pwd").value != document.querySelector("#cnfpwd").value) {
+      document.querySelector("#cnfpwd").setCustomValidity("Passwords Don't Match");
+      document.querySelector("#cnfpwd").reportValidity();
+      document.querySelector("#pwd").value ='';
+      document.querySelector("#cnfpwd").value ='';
+    } 
+    else {
+      document.querySelector("#cnfpwd").setCustomValidity('');
+      document.querySelector("#cnfpwd").reportValidity();
+    } 
+  }
+    
+
+
+
 
 
 
